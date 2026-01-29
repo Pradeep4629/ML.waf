0e5b0a80c4e67dfce05804c1470b9bea4f24302b
Pradeep4629 <143999637+Pradeep4629@users.noreply.github.com>
2026-01-29 14:57:12 +0530

    Modify app run configuration to use PORT env variable
    Update app.py to use environment variable for port.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask
 from src.hybrid_waf.routes.main import main_bp
-from src.hybrid_waf.routes.proxy import proxy_bp  # Import proxy Blueprint
+from src.hybrid_waf.routes.proxy import proxy_bp
 
 app = Flask(__name__)
 
@@ -8,5 +9,7 @@ app = Flask(__name__)
 app.register_blueprint(main_bp)
 app.register_blueprint(proxy_bp)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
